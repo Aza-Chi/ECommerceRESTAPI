6f21b512d309719b447dd08acf9a5f842f486a21
Chi <azarashibobobo@gmail.com>
2024-05-23 23:08:07 +0100

    Add orderdetails/routes.js

---
+++ src/orderdetails/routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const router = Router();
+const controller = require('./controller');
+
+//get
+//post
+//put
+//delete 
+
+//export

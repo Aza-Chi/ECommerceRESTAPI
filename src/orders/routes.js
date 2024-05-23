497c697f8361de9cde8e41592599e97bbce285ba
Chi <azarashibobobo@gmail.com>
2024-05-23 23:10:30 +0100

    update orders/routes.js

--- src/orders/routes.js
+++ src/orders/routes.js
@@ -6,8 +6,10 @@ const controller = require('./controller');
 router.get('/', controller.getOrders);
 router.get('/:order_id', controller.getOrderById);
 
+//If order is canceled
 router.delete('/:order_id', controller.removeOrder);
 
+//checkout processed -> creates order 
 router.post('/', [
     check('customer_id').exists().isInt(),
     check('total_amount').exists().isNumeric(),
@@ -15,6 +17,7 @@ router.post('/', [
     check('status_id').exists().isInt(),
   ], controller.addOrder);
 
+  //The order has already been made, maybe only able to change the address before status progress?
 router.put('/:order_id', [
     check('customer_id').optional().isInt(),
     check('total_amount').optional().isNumeric(),

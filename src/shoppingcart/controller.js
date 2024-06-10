c35db986d9a42283e6190d852da9c3cdce8947aa
Chi <azarashibobobo@gmail.com>
2024-06-10 20:41:45 +0100

    formatting - shoppingcart

--- src/shoppingcart/controller.js
+++ src/shoppingcart/controller.js
@@ -1,130 +1,124 @@
-const pool = require('../../db');
-const queries = require('./queries');
-
-
+const pool = require("../../db");
+const queries = require("./queries");
 
 const getCarts = (req, res) => {
-    console.log("Fetching shopping cart...");
-    pool.query(queries.getCarts, (error, results) => {
-        if (error) {
-            console.error('Error fetching shopping cart:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-                console.log("Shopping cart fetched successfully.");
-                res.status(200).json(results.rows);
-            
-        }
-    });
+  console.log("Fetching shopping cart...");
+  pool.query(queries.getCarts, (error, results) => {
+    if (error) {
+      console.error("Error fetching shopping cart:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log("Shopping cart fetched successfully.");
+      res.status(200).json(results.rows);
+    }
+  });
 };
 
 const getCartByCustomerId = (req, res) => {
-    const customerId = parseInt(req.params.customer_id);
-    pool.query(queries.getCartByCustomerId, [customerId], (error, results) => {
-        if (error) {
-            console.error('Error fetching shopping carts for customer ID:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-            res.status(200).json(results.rows);
-        }
-    });
+  const customerId = parseInt(req.params.customer_id);
+  pool.query(queries.getCartByCustomerId, [customerId], (error, results) => {
+    if (error) {
+      console.error("Error fetching shopping carts for customer ID:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      res.status(200).json(results.rows);
+    }
+  });
 };
 
 const addCart = async (req, res) => {
-    const { customer_id, product_id, quantity } = req.body;
+  const { customer_id, product_id, quantity } = req.body;
 
-    try {
-        await pool.query(queries.addCart, [customer_id, product_id, quantity]);
-        console.log("Shopping cart added successfully!");
-        res.status(201).send("Shopping cart added successfully!");
-    } catch (error) {
-        console.error("Error adding shopping cart:", error);
-        res.status(500).send("Error adding shopping cart.");
-    }
+  try {
+    await pool.query(queries.addCart, [customer_id, product_id, quantity]);
+    console.log("Shopping cart added successfully!");
+    res.status(201).send("Shopping cart added successfully!");
+  } catch (error) {
+    console.error("Error adding shopping cart:", error);
+    res.status(500).send("Error adding shopping cart.");
+  }
 };
 
 const removeCart = (req, res) => {
-    const cart_id = parseInt(req.params.cart_id);
-    
-    // Check if the cart entry exists before attempting removal
-    pool.query(queries.checkCartExists, [cart_id], (error, results) => {
-        if (error) {
-            console.error('Error checking if cart entry exists:', error);
-            return res.status(500).send("Error checking if cart entry exists.");
-        }
+  const cart_id = parseInt(req.params.cart_id);
 
-        // If no cart entry is found with the given ID, return a 404 response
-        if (results.rows.length === 0) {
-            return res.status(404).send("Cart entry does not exist in database!");
-        }
-
-        // Remove the cart entry
-        pool.query(queries.removeCart, [cart_id], (error, results) => {
-            if (error) {
-                console.error('Error removing cart entry:', error);
-                return res.status(500).send("Error removing cart entry.");
-            }
-            res.status(200).send("Cart Entry Successfully Removed");
-        });
-    });
-};
-
-//////////
-   
-
-
-//////////
-
-
-
-const updateCartColumn = async (req, res) => { //CHANGE TO COALESCE?
-    const { customer_id, product_id, quantity } = req.body;
-
-    try {
-        // Check if the cart entry exists before attempting to update
-        const result = await pool.query(queries.getCartByCustomerId, [customer_id]);
-
-        // If no cart entry is found with the given IDs, return a 404 response
-        if (result.rows.length === 0) {
-            return res.status(404).send("Cart entry does not exist in database!");
-        }
-
-        // Update the cart entry columns based on the request body
-        const updates = [
-            { column: 'customer_id', value: customer_id },
-            { column: 'product_id', value: product_id },
-            { column: 'quantity', value: quantity }
-        ];
-
-        for (const update of updates) {
-            if (update.value !== undefined) {
-                const updateQuery = queries.generateUpdateQuery(update.column); // Generate update query for the column
-                await updateColumn(updateQuery, update.value, result.rows[0].cart_id); // Update the column (Changed the parameters passed to updateColumn)
-            }
-        }
-
-        res.status(200).send("Columns updated successfully");
-    } catch (error) {
-        console.error("Error updating columns:", error);
-        res.status(500).send("Error updating columns.");
+  // Check if the cart entry exists before attempting removal
+  pool.query(queries.checkCartExists, [cart_id], (error, results) => {
+    if (error) {
+      console.error("Error checking if cart entry exists:", error);
+      return res.status(500).send("Error checking if cart entry exists.");
     }
+
+    // If no cart entry is found with the given ID, return a 404 response
+    if (results.rows.length === 0) {
+      return res.status(404).send("Cart entry does not exist in database!");
+    }
+
+    // Remove the cart entry
+    pool.query(queries.removeCart, [cart_id], (error, results) => {
+      if (error) {
+        console.error("Error removing cart entry:", error);
+        return res.status(500).send("Error removing cart entry.");
+      }
+      res.status(200).send("Cart Entry Successfully Removed");
+    });
+  });
 };
 
+//////////
+
+//////////
+
+const updateCartColumn = async (req, res) => {
+  //CHANGE TO COALESCE?
+  const { customer_id, product_id, quantity } = req.body;
+
+  try {
+    // Check if the cart entry exists before attempting to update
+    const result = await pool.query(queries.getCartByCustomerId, [customer_id]);
+
+    // If no cart entry is found with the given IDs, return a 404 response
+    if (result.rows.length === 0) {
+      return res.status(404).send("Cart entry does not exist in database!");
+    }
+
+    // Update the cart entry columns based on the request body
+    const updates = [
+      { column: "customer_id", value: customer_id },
+      { column: "product_id", value: product_id },
+      { column: "quantity", value: quantity },
+    ];
+
+    for (const update of updates) {
+      if (update.value !== undefined) {
+        const updateQuery = queries.generateUpdateQuery(update.column); // Generate update query for the column
+        await updateColumn(updateQuery, update.value, result.rows[0].cart_id); // Update the column (Changed the parameters passed to updateColumn)
+      }
+    }
+
+    res.status(200).send("Columns updated successfully");
+  } catch (error) {
+    console.error("Error updating columns:", error);
+    res.status(500).send("Error updating columns.");
+  }
+};
 
 // Function to update a specific column
-const updateColumn = async (updateQuery, value, customer_id) => { // Pass the generated query as a parameter
-    try {
-        await pool.query(updateQuery, [value, customer_id]);
-        console.log(`Column updated successfully`);
-    } catch (error) {
-        console.error(`Error updating column:`, error);
-        throw error;
-    }
+const updateColumn = async (updateQuery, value, customer_id) => {
+  // Pass the generated query as a parameter
+  try {
+    await pool.query(updateQuery, [value, customer_id]);
+    console.log(`Column updated successfully`);
+  } catch (error) {
+    console.error(`Error updating column:`, error);
+    throw error;
+  }
 };
 
 module.exports = {
-    getCarts,
-    getCartByCustomerId,
-    addCart,
-    removeCart,
-    updateCartColumn,
+  getCarts,
+  getCartByCustomerId,
+  addCart,
+  removeCart,
+  updateCartColumn,
 };
--- src/shoppingcart/queries.js
+++ src/shoppingcart/queries.js
@@ -1,24 +1,22 @@
 const getCarts = "SELECT * FROM shoppingcart";
-const getCartByCustomerId = "SELECT * FROM shoppingcart WHERE customer_id = $1"; //Customer id //Limit customers so they can only see their carts !!! 
-const checkCartExists = "SELECT i FROM shoppingcart i WHERE i.cart_id = $1"; // i is the alias 
-const addCart = "INSERT INTO shoppingcart (customer_id, product_id, quantity) VALUES ($1, $2, $3)"; //1. Add products to cart !!!
+const getCartByCustomerId = "SELECT * FROM shoppingcart WHERE customer_id = $1"; //Customer id //Limit customers so they can only see their carts !!!
+const checkCartExists = "SELECT i FROM shoppingcart i WHERE i.cart_id = $1"; // i is the alias
+const addCart =
+  "INSERT INTO shoppingcart (customer_id, product_id, quantity) VALUES ($1, $2, $3)"; //1. Add products to cart !!!
 const removeCart = "DELETE FROM shoppingcart WHERE cart_id = $1";
-const removeCartByCustomerId = "DELETE FROM shoppingcart WHERE customer_id = $1";
-
+const removeCartByCustomerId =
+  "DELETE FROM shoppingcart WHERE customer_id = $1";
 
 const generateUpdateQuery = (column) => {
-    return `UPDATE shoppingcart SET ${column} = $1, updated_at = NOW() WHERE cart_id = $2`;
-  };
-
-
-module.exports = {
-    getCarts,   
-    getCartByCustomerId,
-    checkCartExists,
-    addCart,
-    removeCart,
-    removeCartByCustomerId,
-    generateUpdateQuery,
+  return `UPDATE shoppingcart SET ${column} = $1, updated_at = NOW() WHERE cart_id = $2`;
 };
 
-
+module.exports = {
+  getCarts,
+  getCartByCustomerId,
+  checkCartExists,
+  addCart,
+  removeCart,
+  removeCartByCustomerId,
+  generateUpdateQuery,
+};

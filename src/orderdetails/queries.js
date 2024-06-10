ecb802797cbc90d3c77352aff207623efe96356a
Chi <azarashibobobo@gmail.com>
2024-06-10 19:43:02 +0100

    formatting - orderdetails

--- src/orderdetails/queries.js
+++ src/orderdetails/queries.js
@@ -1,21 +1,24 @@
 const getOrderDetails = "SELECT * FROM orderdetails";
-const getOrderDetailsById = "SELECT * FROM orderdetails WHERE order_detail_id = $1";
-const checkOrderDetailsExist = "SELECT i FROM orderdetails i WHERE i.order_detail_id = $1"; // i is the alias 
-const addOrderDetails = "INSERT INTO orderdetails (order_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)"; 
-const removeOrderDetails = "DELETE FROM orderdetails WHERE order_detail_id = $1";
+const getOrderDetailsById =
+  "SELECT * FROM orderdetails WHERE order_detail_id = $1";
+const checkOrderDetailsExist =
+  "SELECT i FROM orderdetails i WHERE i.order_detail_id = $1"; // i is the alias
+const addOrderDetails =
+  "INSERT INTO orderdetails (order_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)";
+const removeOrderDetails =
+  "DELETE FROM orderdetails WHERE order_detail_id = $1";
 
 const generateUpdateQuery = (column) => {
-    return `UPDATE orderdetails SET ${column} = $1 WHERE order_id = $2`;
-  };
+  return `UPDATE orderdetails SET ${column} = $1 WHERE order_id = $2`;
+};
 
-
-  //Subtotal calculated as quantity*product_id(price) in products
+//Subtotal calculated as quantity*product_id(price) in products
 
 module.exports = {
-    getOrderDetails,   
-    getOrderDetailsById,
-    checkOrderDetailsExist,
-    addOrderDetails,
-    removeOrderDetails,
-    generateUpdateQuery,
+  getOrderDetails,
+  getOrderDetailsById,
+  checkOrderDetailsExist,
+  addOrderDetails,
+  removeOrderDetails,
+  generateUpdateQuery,
 };
--- src/orderdetails/routes.js
+++ src/orderdetails/routes.js
@@ -1,27 +1,33 @@
-const { Router } = require('express');
-const { check } = require('express-validator');
+const { Router } = require("express");
+const { check } = require("express-validator");
 const router = Router();
-const controller = require('./controller');
+const controller = require("./controller");
 
-router.get('/', controller.getOrderDetails);
-router.get('/:order_detail_id', controller.getOrderDetailsById);
+router.get("/", controller.getOrderDetails);
+router.get("/:order_detail_id", controller.getOrderDetailsById);
 
-router.post('/', [
-    check('order_id').exists().isInt(),
-    check('product_id').exists().isInt(),
-    check('quantity').exists().isInt(),
-    check('subtotal').exists().isNumeric(),
-  ], controller.addOrderDetails);
+router.post(
+  "/",
+  [
+    check("order_id").exists().isInt(),
+    check("product_id").exists().isInt(),
+    check("quantity").exists().isInt(),
+    check("subtotal").exists().isNumeric(),
+  ],
+  controller.addOrderDetails
+);
 //put
-router.put('/:order_detail_id', [
-    check('order_id').optional().isInt(),
-    check('product_id').optional().isInt(),
-    check('quantity').optional().isInt(),
-    check('subtotal').optional().isNumeric(),
-  ], controller.updateOrderDetails);
+router.put(
+  "/:order_detail_id",
+  [
+    check("order_id").optional().isInt(),
+    check("product_id").optional().isInt(),
+    check("quantity").optional().isInt(),
+    check("subtotal").optional().isNumeric(),
+  ],
+  controller.updateOrderDetails
+);
 
-
-router.delete('/:order_detail_id', controller.removeOrderDetails);
-
+router.delete("/:order_detail_id", controller.removeOrderDetails);
 
 module.exports = router;

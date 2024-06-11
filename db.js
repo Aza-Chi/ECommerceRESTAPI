bfd93649f3be8a9b3d8fdbff57003ca6254b17e7
Chi <azarashibobobo@gmail.com>
2024-06-11 11:55:37 +0100

    formatting - db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 const pool = new Pool({
-user: "postgres",
-host: "localhost",
-database: "store",
-password: "postgres",
-port: 5432,
+  user: "postgres",
+  host: "localhost",
+  database: "store",
+  password: "postgres",
+  port: 5432,
 });
 
 module.exports = pool;

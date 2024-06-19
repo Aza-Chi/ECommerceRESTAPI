b053ab11031d9eba226ce64764a6695e012adaeb
Chi <azarashibobobo@gmail.com>
2024-06-19 23:43:45 +0100

    updated auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -39,21 +39,58 @@ const register = async (req, res) => {
   }
 };
 
+
+
+// Token stuffs -  passport sessions not working!/session id keep changing no matter what
+// so let's do it this way for now!
+function authenticateToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    console.log("No token provided, proceed with limited or public data");
+    req.user = null;
+    next();
+    return;
+  }
+//Needed token.split(' ')[1] to get rid of Bearer prefix!!!!!!!!
+  jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.error("Token verification failed:", err);
+      req.user = null;
+      next();
+      return;
+    }
+    console.log("Token is valid, attach user information to req.user");
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = {
+  register,
+  authenticateToken,
+  //login, - it became a passport local strategy instead!
+};
+
+
+/* login without passport ! Reworking this to use passport!!
 const login = async (req, res) => {
   const { usernameOrEmail, password } = req.body;
+  console.log("Server received the following data:");
+    console.log(`usernameOrEmail: ${usernameOrEmail} and password: ${password}`);
   try {
+    
     // Check if the usernameOrEmail corresponds to a username
-    let result = await pool.query(queries.checkUsernameExists, [
-      usernameOrEmail,
-    ]);
+    let result = await pool.query(queries.checkUsernameExists, [usernameOrEmail]);
     console.log("Username Query Result:", result.rows);
-    let user = parseUser(result.rows[0].c);
+
+    let user = result.rows.length > 0 ? parseUser(result.rows[0].c) : null;
 
     // If no user found, check if the usernameOrEmail corresponds to an email
     if (!user) {
       result = await pool.query(queries.checkEmailExists, [usernameOrEmail]);
       console.log("Email Query Result:", result.rows);
-      user = parseUser(result.rows[0].c);
+      user = result.rows.length > 0 ? parseUser(result.rows[0].c) : null;
     }
 
     // Log user information
@@ -69,7 +106,7 @@ const login = async (req, res) => {
           process.env.JWT_SECRET,
           { expiresIn: "1h" }
         );
-        console.log("Token: " + { token });
+        console.log("Token: ", token);
         res.json({ token });
       } else {
         console.log("Failed to Login!");
@@ -84,40 +121,20 @@ const login = async (req, res) => {
     res.status(500).send("Server error");
   }
 };
+*/
 
+/*
+//Moved to passportConfig.js
 // Parse user string
 function parseUser(userString) {
-  if (!userString) return null; //userString is null or empty?
+  if (!userString) return null; // userString is null or empty?
   const userData = userString.slice(1, -1).split(","); // Split userString into an array of fields ',' = delimiter
   return {
-    id: userData[0], //index is field column
+    id: userData[0], // index is field column
     username: userData[1],
     password: userData[2],
-    email: userData[5], //5th column in table is email etc
+    email: userData[5], // 5th column in table is email etc
   };
 }
 
-// Token stuffs - Could also use passport for authentication middleware but let's continue it this way for now
-
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).send("Access denied. No token provided.");
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).send("Invalid token.");
-    }
-    req.user = user;
-    next();
-  });
-};
-
-module.exports = {
-  register,
-  login,
-  authenticateToken,
-};
+*/

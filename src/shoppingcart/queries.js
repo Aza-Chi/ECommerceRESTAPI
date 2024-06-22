017c6a997488329d3a0f6c11b2018c2a9708bf27
Chi <azarashibobobo@gmail.com>
2024-06-22 12:08:29 +0100

    updated shoppingcart/queries.js added product joins to get queries

--- src/shoppingcart/queries.js
+++ src/shoppingcart/queries.js
@@ -1,5 +1,16 @@
-const getCarts = "SELECT * FROM shoppingcart";
-const getCartByCustomerId = "SELECT * FROM shoppingcart WHERE customer_id = $1"; //Customer id //Limit customers so they can only see their carts !!!
+const getCarts = `
+  SELECT sc.cart_id, sc.customer_id, sc.product_id, sc.quantity, p.product_name, p.price
+  FROM shoppingcart sc
+  JOIN products p ON sc.product_id = p.product_id
+`;
+// const getCarts = "SELECT * FROM shoppingcart"; // OLD!
+const getCartByCustomerId = `
+  SELECT sc.cart_id, sc.customer_id, sc.product_id, sc.quantity, p.product_name, p.price
+  FROM shoppingcart sc
+  JOIN products p ON sc.product_id = p.product_id
+  WHERE sc.customer_id = $1
+`;// JOIN with products table so we can get the product name too!!!
+//const getCartByCustomerId = "SELECT * FROM shoppingcart WHERE customer_id = $1";// OLD  //Customer id //Limit customers so they can only see their carts !!!
 const checkCartExists = "SELECT i FROM shoppingcart i WHERE i.cart_id = $1"; // i is the alias
 const addCart =
   "INSERT INTO shoppingcart (customer_id, product_id, quantity) VALUES ($1, $2, $3)"; //1. Add products to cart !!!
